fdcc04c3116bbe631bc9b12bfa02e976b9837b3b
yukinissie <yuki.nissie.hoshi@gmail.com>
2020-04-27 19:19:14 +0900

    add img

--- service_worker.js
+++ service_worker.js
@@ -1,27 +1,27 @@
 // キャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
+var CACHE_NAME = 'pwa-caches';
 var urlsToCache = [
-    '/poster-keisuke.github.io/',
+  '/yukinissie.github.io/',
 ];
 
 // インストール処理
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 // リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(function(response) {
+        return response ? response : fetch(event.request);
+      })
+  );
 });
