39f3ae91a1fe8c842667d752ec375b695638629d
yukinissie <yuki.nissie.hoshi@gmail.com>
2020-04-28 10:17:45 +0900

    Cache Google Fonts

--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,33 @@
+import {registerRoute} from 'workbox-routing';
+import {CacheFirst, StaleWhileRevalidate} from 'workbox-strategies';
+import {CacheableResponsePlugin} from 'workbox-cacheable-response';
+import {ExpirationPlugin} from 'workbox-expiration';
+
+// Cache the Google Fonts stylesheets with a stale while revalidate strategy.
+registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  }),
+);
+
+
+// Cache the Google Fonts webfont files with a cache first strategy for 1 year.
+registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+      }),
+    ],
+  }),
+);
+
 // キャッシュファイルの指定
 var CACHE_NAME = 'pwa-caches';
 var urlsToCache = [
